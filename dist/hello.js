ca16a894b2984f902688d4dff6e7c14aea2b6995
aa <aac1291@gmx.ch>
2022-11-03 14:18:13 +0100

    dist

---
+++ dist/hello.js
@@ -0,0 +1,8 @@
+"use strict";
+class hello {
+    static say(o) {
+        const input = document.getElementById('1');
+        input.innerHTML += " Hello";
+    }
+}
+//# sourceMappingURL=hello.js.map
